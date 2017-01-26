463f5f25beb08bdc956b6f462b6a4eb27a012769
smre <eino.keningi@gmail.com>
2017-01-26 21:09:14 +0200

    Changed button style location

--- js/numbering.js
+++ js/numbering.js
@@ -1,20 +1,20 @@
 var SITE = {
-    init: function () {
+    init: function() {
         SITE.initToc();
     },
-    initToc: function () {
-        $(".tehtava").each(function (index, value) {
+    initToc: function() {
+        $(".tehtava").each(function(index, value) {
             if ($('#chapterNumber').val() == null) {
                 var chNumber = "";
             } else {
                 var chNumber = parseInt($('#chapterNumber').val());
             }
-
+            
             var exCount = (index + 1);
             var problemNumber = chNumber + "." + exCount;
             var exName = "Tehtävä " + problemNumber + ": " + $(value).find("h1 a").text();
 
-            // add assignments to toc
+             // add assignments to toc 
 //            $("#tehtavat-toc").append("<li><a data-toggle='collapse' href='" + $(value).find("h1 a").attr("href") + "'>" + exName + "</a></li>");
 
             // add links to assignment names
@@ -38,8 +38,8 @@ var SITE = {
 
             // Insert button group here
             var $input = $('<div class="problemButtonWrap"><h3 id="textbar_' + problemID + '">Miten tehtävä meni?</h3><div class="btn-group btn-group"><button data-toggle="tooltip" title="En osannut tehtävää. Tarvitsen apua." class="problemButton btn btn-danger btn-primary" id=' + "0;" + problemID + '><img class="face" src="/img/faces/sad.svg"></button>'
-                + '<button data-toggle="tooltip" title="Ratkaisin tehtävän, mutta olen epävarma vastauksesta." class="problemButton btn btn-warning btn-primary" id=' + "1;" + problemID + '><img class="face" src="/img/faces/meh.svg"></button>'
-                + '<button data-toggle="tooltip" title="Ratkaisin tehtävän ja osaan tämän." class="problemButton btn btn-success btn-primary" id=' + "2;" + problemID + '><img class="face" src="/img/faces/happy.svg"></button></div></div>');
+                        + '<button data-toggle="tooltip" title="Ratkaisin tehtävän, mutta olen epävarma vastauksesta." class="problemButton btn btn-warning btn-primary" id=' + "1;" + problemID + '><img class="face" src="/img/faces/meh.svg"></button>'
+                        + '<button data-toggle="tooltip" title="Ratkaisin tehtävän ja osaan tämän." class="problemButton btn btn-success btn-primary" id=' + "2;" + problemID + '><img class="face" src="/img/faces/happy.svg"></button></div></div>');
             $(value).find("div").append($input);
 
 
@@ -52,18 +52,18 @@ var SITE = {
             $(value).attr("id", problemID);
 
             // tag subassignments
-            $(value).find("div h1").each(function (subIndex, value) {
+            $(value).find("div h1").each(function(subIndex, value) {
                 $(value).text(exCount + "." + (subIndex + 1) + ": " + $(value).text());
             });
         });
-
+        
         if ($('#theoremStart').val() == null) {
-            var thStart = 1;
-        } else {
-            var thStart = parseInt($('#theoremStart').val());
-        }
-
-        $(".theorem").each(function (thIndex, value) {
+                var thStart = 1;
+            } else {
+                var thStart = parseInt($('#theoremStart').val());
+            }
+        
+         $(".theorem").each(function(thIndex, value) {
 
             var thCount = (thStart + thIndex);
             var thName = "TEOREEMA " + thCount;
@@ -72,85 +72,85 @@ var SITE = {
             if (this.id === '') {
                 $(value).find("h3").text(thName);
             } else {
-                $(value).find("h3").text(this.id + " (" + thName + ")");
+                $(value).find("h3").text(this.id + " (" + thName +")");
             }
         });
+     
+/*
+        // link toc to assignments
+        $("#tehtavat-toc a").each(function(index, value) {
+            $(value).click(function() {
+                $('html, body').animate({
+                    scrollTop: $($.attr(this, 'href') + "-ex").offset().top
+                }, 400);
 
-        /*
-         // link toc to assignments
-         $("#tehtavat-toc a").each(function(index, value) {
-         $(value).click(function() {
-         $('html, body').animate({
-         scrollTop: $($.attr(this, 'href') + "-ex").offset().top
-         }, 400);
+                $($.attr(this, 'href')).click();
+            });
+        });
 
-         $($.attr(this, 'href')).click();
-         });
-         });
+        var idx = 1;
+        $("section h1").each(function(index, value) {
+            if ($(value).parents('.tehtava').length) {
+                return; // ignore assignments
+            }
 
-         var idx = 1;
-         $("section h1").each(function(index, value) {
-         if ($(value).parents('.tehtava').length) {
-         return; // ignore assignments
-         }
+            if ($(value).parents('.no-toc').length) {
+                return; //ignore sections with .no-toc
+            }
 
-         if ($(value).parents('.no-toc').length) {
-         return; //ignore sections with .no-toc
-         }
-
-         if ($(value).parent('section').length) {
-
-
-         var chapterCount = idx;
-
-         var chapterText = chapterCount + ". " + $(value).text();
-
-         console.log(chapterText);
-
-         $(value).attr("id", "chapter" + chapterCount);
-         $(value).text(chapterText);
-
-         // add chapters to toc
-         $("#material-toc").append("<li><a href='#chapter" + chapterCount + "'>" + chapterText + "</a></li>");
-         idx++;
-
-         // iterate through siblings
-         var sibling = $(value).next();
-         var count = 1;
-         while (sibling) {
-         // do not relabel assignments
-         if (!$(sibling).prop("tagName") || $(sibling).prop("tagName").toLowerCase() === "h1") {
-         break;
-         }
-
-         if ($(sibling).prop("tagName").toLowerCase() === "h2") {
-         var subChapterText = (chapterCount + "." + count + ". " + $(sibling).text());
-         $(sibling).text(subChapterText);
-         var id = "chapter" + chapterCount + "-" + count;
-         $(sibling).attr("id", id);
-
-         $("#material-toc").append("<li><a href='#" + id + "'>&nbsp;&nbsp;&nbsp;" + subChapterText + "</a></li>");
-
-         count++;
-         }
+            if ($(value).parent('section').length) {
 
 
-         sibling = $(sibling).next();
+                var chapterCount = idx;
 
-         if(sibling.length <= 0) {
-         break;
-         }
-         }
-         }
+                var chapterText = chapterCount + ". " + $(value).text();
+                
+                console.log(chapterText);
 
-         });
-         */
-    }
+                $(value).attr("id", "chapter" + chapterCount);
+                $(value).text(chapterText);
+
+                // add chapters to toc 
+                $("#material-toc").append("<li><a href='#chapter" + chapterCount + "'>" + chapterText + "</a></li>");
+                idx++;
+
+                // iterate through siblings
+                var sibling = $(value).next();
+                var count = 1;
+                while (sibling) {
+                    // do not relabel assignments
+                    if (!$(sibling).prop("tagName") || $(sibling).prop("tagName").toLowerCase() === "h1") {
+                        break;
+                    }
+
+                    if ($(sibling).prop("tagName").toLowerCase() === "h2") {
+                        var subChapterText = (chapterCount + "." + count + ". " + $(sibling).text());
+                        $(sibling).text(subChapterText);
+                        var id = "chapter" + chapterCount + "-" + count;
+                        $(sibling).attr("id", id);
+                        
+                        $("#material-toc").append("<li><a href='#" + id + "'>&nbsp;&nbsp;&nbsp;" + subChapterText + "</a></li>");
+                        
+                        count++;
+                    }
+
+
+                    sibling = $(sibling).next();
+                    
+                    if(sibling.length <= 0) {
+                        break;
+                    }
+                }
+            }
+
+        });
+  */
+    }  
 
 };
 
 
-$(function () {
+$(function() {
     console.log("Init site");
     SITE.init();
 });
