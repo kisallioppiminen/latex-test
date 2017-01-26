19a79588d2c67f0781351ff9b302ae4789998239
smre <eino.keningi@gmail.com>
2017-01-26 20:42:18 +0200

    Buttons with appearing text

--- js/button/button.js
+++ js/button/button.js
@@ -2,7 +2,11 @@ $( document ).ready(function() {
     console.log( "Button.js ready" );
     $('.problemButton').click(function() {
         console.log("Button " + this.id + " pressed.");
-        /*  Send POST to server */
+        /*  Send POST request here */
+
+        /* Change button title text */
+        var text_id = 'h3[id="textbar_' + this.id.substr(2,this.id.length - 1) + '"]';
+        $(text_id).html("Vastauksesi on lähetetty!");
     });
 });
 
