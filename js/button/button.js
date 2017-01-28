8c18b1226032d229dd26bc5de0b917cfe6d4c9bb
smre <eino.keningi@gmail.com>
2017-01-28 13:27:43 +0200

    Problem header changes color when button is pressed

--- js/button/button.js
+++ js/button/button.js
@@ -1,3 +1,12 @@
+function changeProblemHeaderColor(id) {
+    var problemID = id.substr(2, id.length - 1);
+    var colors = ["#d9534f", "#f0ad4e", "#5cb85c"];
+
+    var header_id = 'div[id="' + problemID + '"]';
+
+    $(header_id + " header ").css({"background" : colors[id.charAt(0)]});
+}
+
 $( document ).ready(function() {
     console.log( "Button.js ready" );
     $('.problemButton').click(function() {
@@ -7,6 +16,8 @@ $( document ).ready(function() {
         /* Change button title text */
         var text_id = 'h3[id="textbar_' + this.id.substr(2,this.id.length - 1) + '"]';
         $(text_id).html("Vastauksesi on lähetetty!");
+
+        changeProblemHeaderColor(this.id);
     });
 });
 
