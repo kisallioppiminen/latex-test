95c24f950ddd8baebb00b818a8f41cf3afcd73ac
smre <eino.keningi@gmail.com>
2017-01-30 22:07:52 +0200

    Fix deprecated async and compatibility issues

--- js/scoreboard/scoreboard.js
+++ js/scoreboard/scoreboard.js
@@ -1,36 +1,40 @@
 $( document ).ready(function() {
 
-    console.log("Scoreboard.js loaded");
+    // Creates tables
+    function createTable(coursesJSON) {
+        var courseName = "maa1";
+        var rows = '';
+        rows += '<tr class="success"><th></th>';
+        Object.keys(coursesJSON[courseName]["john"]).forEach(function(exerciseName) {
+            rows += '<th>' + exerciseName + '</th>';
+        });
 
-    var coursesJSON = $.parseJSON(
-        $.ajax({
-            url: 'https://pure-inlet-98383.herokuapp.com/course/1/checkmarks',
-            async: false,
-            dataType: 'json',
-        }).responseText
-    );
+        rows += '</tr>';
+        Object.keys(coursesJSON[courseName]).forEach(function(studentName) {
+            rows += '<tr id = "row' + studentName + '">' +
+                '<td>' + studentName + '</td>';
 
-    console.log(coursesJSON);
+            var problems = coursesJSON[courseName][studentName];
+            for (var key in problems) {
+                var value = problems[key];
+                rows += '<td style="background-color:' + value + '">' + value + '</td>'
+            }
 
-    var courseName = "maa1";
-    var rows = '';
-    rows += '<tr class="success"><th></th>';
-    Object.keys(coursesJSON[courseName]["john"]).forEach(function(exerciseName) {
-        rows += '<th>' + exerciseName + '</th>';
+            '</tr>';
+        });
+
+        document.getElementById( 'courseHeader1').innerHTML = courseName;
+        document.getElementById( 'table' ).innerHTML = rows;
+    }
+
+    // GET happens here
+    $.ajax({
+        url: 'https://pure-inlet-98383.herokuapp.com/course/1/checkmarks',
+        dataType: 'json',
+        success: function(data) {
+            createTable(data);
+        },
     });
-
-    rows += '</tr>';
-    Object.keys(coursesJSON[courseName]).forEach(function(studentName) {
-        rows += '<tr id = "row' + studentName + '">' +
-            '<td>' + studentName + '</td>';
-        Object.entries(coursesJSON[courseName][studentName]).forEach(
-            ([key, value]) => rows += '<td style="background-color:' + value + '">' + value + '</td>'
-        );
-        '</tr>';
-    });
-
-    document.getElementById( 'courseHeader1').innerHTML = courseName;
-    document.getElementById( 'table' ).innerHTML = rows;
 
 });
 
