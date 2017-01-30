eecb3d3c144c59686dc1a00d89f71a8c00912cca
smre <eino.keningi@gmail.com>
2017-01-30 13:58:21 +0200

    Course manager gets JSON from server

---
+++ js/scoreboard/scoreboard.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+
+    console.log("Scoreboard.js loaded");
+
+    var coursesJSON = $.parseJSON(
+        $.ajax({
+            url: 'https://pure-inlet-98383.herokuapp.com/course/1/checkmarks',
+            async: false,
+            dataType: 'json',
+        }).responseText
+    );
+
+    console.log(coursesJSON);
+
+    var courseName = "maa1";
+    var rows = '';
+    rows += '<tr class="success"><th></th>';
+    Object.keys(coursesJSON[courseName]["john"]).forEach(function(exerciseName) {
+        rows += '<th>' + exerciseName + '</th>';
+    });
+
+    rows += '</tr>';
+    Object.keys(coursesJSON[courseName]).forEach(function(studentName) {
+        rows += '<tr id = "row' + studentName + '">' +
+            '<td>' + studentName + '</td>';
+        Object.entries(coursesJSON[courseName][studentName]).forEach(
+            ([key, value]) => rows += '<td>' + value + '</td>'
+        );
+        '</tr>';
+    });
+
+    document.getElementById( 'courseHeader1').innerHTML = courseName;
+    document.getElementById( 'table' ).innerHTML = rows;
+
+});
+
