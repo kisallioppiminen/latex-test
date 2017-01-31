5c64321f51d43662901a025ddf688768e9ec2f3c
smre <eino.keningi@gmail.com>
2017-01-31 11:02:36 +0200

    Button.js fixes

--- js/button/button.js
+++ js/button/button.js
@@ -1,9 +1,10 @@
 function changeProblemHeaderColor(id) {
     var problemID = id.substr(2, id.length - 1);
+
+    // red, yellow, green
     var colors = ["#d9534f", "#f0ad4e", "#5cb85c"];
 
     var header_id = 'div[id="' + problemID + '"]';
-
     $(header_id + " header ").css({"background" : colors[id.charAt(0)]});
 }
 
@@ -13,10 +14,12 @@ $( document ).ready(function() {
         console.log("Button " + this.id + " pressed.");
         /* Send POST request here */
 
+
         /* Change button title text */
         var text_id = 'h3[id="textbar_' + this.id.substr(2,this.id.length - 1) + '"]';
         $(text_id).html("Vastauksesi on lähetetty!");
 
+        /* Change problem header color */
         changeProblemHeaderColor(this.id);
     });
 });
