6bc3f01f7687a415a23b8a415571359ac00baa1d
Greatman Lim <greatman.lim@mahti.org>
2017-02-12 14:19:36 +0200

    Clear but keep for reference

--- js/session.js
+++ js/session.js
@@ -29,63 +29,44 @@ req.send();
 
 // Don't need async, could be in seperate file?
 
-function logout() {
+// function logout() {
+//   var req = new XMLHttpRequest();
+//   req.onreadystatechange = function() {
+//     if (req.readyState !== this.DONE) {
+//       console.log("state " + req.readyState);
+//       return false;
+//     }
+//     if (req.status !== 200) {
+//       console.log("status " + req.status);
+//       return false;
+//     }
+//     if (req.status === 204) {
+//       console.log("status " + req.status);
+//       console.log("Loggin user out!")
+//       return false;
+//     }
+//     console.log("Logging out!")
+//   }
+//   req.open("GET", BACKEND_BASE_URL + "users/sign_out", true);
+//   req.withCredentials = true;
+//   req.send();
+// }
 
-  // const deleteURL = BACKEND_BASE_URL + "users/sign_out"
-
-  // $.ajax({
-  //     url: deleteURL,
-  //     type: 'DELETE',
-  //     success: function(result) {
-  //       console.log("Are we logged out?" + result)
-  //     }
-  //     error: function(err) {
-  //       console.log("We got some err" + err)
-  //     }
-  // });
-
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function() {
-    if (req.readyState !== this.DONE) {
-      console.log("state " + req.readyState);
-      return false;
-    }
-    if (req.status !== 200) {
-      console.log("status " + req.status);
-      return false;
-    }
-    if (req.status === 204) {
-      console.log("status " + req.status);
-      console.log("Loggin user out!")
-      return false;
-    }
-    console.log("Logging out!")
-  }
-  req.open("DELETE", BACKEND_BASE_URL + "users/sign_out", true);
-  // /users/sign_out(.:format) 
-  req.withCredentials = true;
-  req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  req.send();
-}
-
-function login() {
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function() {
-    if (req.readyState !== this.DONE) {
-      console.log("state " + req.readyState);
-      return false;
-    }
-    if (req.status !== 200) {
-      console.log("status " + req.status);
-      return false;
-    }
-    console.log("Pass to the loggin!")
-  }
-  req.open("POST", BACKEND_BASE_URL + "users/sign_in", true);
-  // /users/sign_out(.:format) 
-  req.withCredentials = true;
-  req.send();
-}
-
-
-
+// function login() {
+//   var req = new XMLHttpRequest();
+//   req.onreadystatechange = function() {
+//     if (req.readyState !== this.DONE) {
+//       console.log("state " + req.readyState);
+//       return false;
+//     }
+//     if (req.status !== 200) {
+//       console.log("status " + req.status);
+//       return false;
+//     }
+//     console.log("Pass to the loggin!")
+//   }
+//   req.open("POST", BACKEND_BASE_URL + "users/sign_in", true);
+//   // /users/sign_out(.:format) 
+//   req.withCredentials = true;
+//   req.send();
+// }
