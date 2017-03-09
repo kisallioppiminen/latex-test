8c899a85d93680371d34fd9f005bc2dfa11019b9
smre <eino.keningi@gmail.com>
2017-03-10 00:01:21 +0200

    Set up ESLint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,6 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-	"jquery": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ]
 };
