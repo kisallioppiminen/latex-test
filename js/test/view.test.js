5f11c83c3e7ef3fd05a6e9415979584c2a2d9530
Greatman Lim <greatman.lim@mahti.org>
2017-03-09 12:10:25 +0200

    Add test skeleton for view

---
+++ js/test/view.test.js
@@ -0,0 +1,16 @@
+/**
+ * @file Unit tests for view module in file view.js.
+ * @license GPL v2
+ * @version 1.00
+ */
+
+describe('View', function() {
+  describe('navigation', function() {
+    beforeEach(function() {
+
+    });
+    afterEach(function() {
+
+    });
+  });
+});
