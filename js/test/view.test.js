53b77595cc9e2572b044ff2b20e2bb8c42707d3c
Greatman Lim <greatman.lim@mahti.org>
2017-03-09 15:46:19 +0200

    Add stories and fixture

--- js/test/view.test.js
+++ js/test/view.test.js
@@ -5,12 +5,30 @@
  */
 
 describe('View', function() {
-  describe('navigation', function() {
+  describe('navigation module', function() {
     beforeEach(function() {
+      // var fixture = '<nav><ul></ul></nav>'
+      // document.body.insertAdjacentHTML('afterbegin', fixture);
 
+      var nav = document.createElement('nav');
+      var ul = document.createElement('ul');
+      nav.appendChild(ul);
+      document.body.appendChild(nav);
     });
     afterEach(function() {
+      var navs = document.getElementsByTagName('nav');
+      document.body.removeChild(navs[0]);
+    });
 
+    it('should be empty if nothing has been built', function() {
+      // var nav = document.getElementsByTagName('nav');
+      // expect(nav.children.length).toBe(1);
+    })
+    it("should have 'Kirjautuminen' in the nav after Guest view has been built", function() {
+      // view.navigation.buildGuest();
+    });
+    it('Should have User view when User view has been build', function() {
+      // view.navigation.buildUser();
     });
   });
 });
