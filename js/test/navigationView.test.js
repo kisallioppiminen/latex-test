667e4d86e30a4762a509752dae95ce06ce7fa046
Greatman Lim <greatman.lim@mahti.org>
2017-03-12 16:33:17 +0200

    Add tests

--- js/test/navigationView.test.js
+++ js/test/navigationView.test.js
@@ -4,34 +4,35 @@
  * @version 1.00
  */
 
-describe('View', function() {
-  describe('navigation module', function() {
-    beforeEach(function() {
-      // var fixture = '<nav><ul></ul></nav>'
-      // document.body.insertAdjacentHTML('afterbegin', fixture);
-
-      var nav = document.createElement('nav');
-      var ul = document.createElement('ul');
-      nav.appendChild(ul);
-      document.body.appendChild(nav);
-      
-      var session = new Session();
-      var navigationview = new NavigationView();
-    });
-    afterEach(function() {
-      var navs = document.getElementsByTagName('nav');
-      document.body.removeChild(navs[0]);
-    });
-
-    it('should be empty if nothing has been built', function() {
-      // var nav = document.getElementsByTagName('nav');
-      // expect(nav.children.length).toBe(1);
-    })
-    it("should have 'Kirjautuminen' in the nav after Guest view has been built", function() {
-      // view.navigation.buildGuest();
-    });
-    it('Should have User view when User view has been build', function() {
-      // view.navigation.buildUser();
-    });
+describe('Navigation view', function() {
+  beforeEach(function() {
+    var nav = document.createElement('nav');
+    var ul = document.createElement('ul');
+    nav.appendChild(ul);
+    document.body.appendChild(nav);
   });
+  afterEach(function() {
+    var navs = document.getElementsByTagName('nav');
+    document.body.removeChild(navs[0]);
+  });
+
+  it('should be empty if nothing has been built', function() {
+    var nav = document.getElementsByTagName('nav');
+    expect(nav.length).toBe(1);
+  })
+  it("should have 'Kirjautuminen' in the nav after Guest view has been built", function() {
+    var navigationview = new NavigationView();
+    navigationview.buildGuest();
+    var links = document.getElementsByTagName('a');
+    expect(links.length).toBe(1);
+    expect(links[0].innerHTML).toBe('Kirjautuminen');
+  });
+  it('Should have User view when User view has been build', function() {
+    // var session = new Session();
+    // var navigationview = new NavigationView(session);
+    // navigationview.buildUser();
+    // var links = document.getElementsByTagName('a');
+    // expect(links.length).toBe(3);
+  });
+
 });
