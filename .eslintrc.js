e8d54ad389f6558e55f0752b4cf1b1d95b8fe800
smre <eino.keningi@gmail.com>
2017-03-14 12:59:02 +0200

    Configure .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,31 @@
 module.exports = {
-    "extends": "eslint:recommended",
-    "plugins": [
-        "import"
-    ],
     "env": {
-    	"jquery": true,
         "browser": true,
-        "jasmine": true
-    }
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-var": "error",
+        "no-console": 0,
+
+    },
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+    },
 };
