a025aae497a421bedd38cb207b1bbc6f0f4d61d7
Greatman Lim <greatman.lim@mahti.org>
2017-03-30 11:17:21 +0300

    Eslint format

--- js/common.js
+++ js/common.js
@@ -1,53 +1,53 @@
 if (typeof HTMLDocument !== 'undefined') {
-    HTMLDocument.prototype.getCookie = function(key) {
-      var value = '; ' + document.cookie;
-      var parts = value.split('; ' + key + '=');
-      if (parts.length == 2) return parts.pop().split(';').shift();
-    }
-    HTMLDocument.prototype.deleteCookie = function(key) {
-      document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    }
+  HTMLDocument.prototype.getCookie = function(key) {
+    let value = '; ' + document.cookie;
+    let parts = value.split('; ' + key + '=');
+    if (parts.length == 2) return parts.pop().split(';').shift();
+  };
+  HTMLDocument.prototype.deleteCookie = function(key) {
+    document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  };
 } else {
   /**
    * @param {String} key - Get cookie by value.
    */
-    Document.prototype.getCookie = function(key) {
-      var value = '; ' + document.cookie;
-      var parts = value.split('; ' + key + '=');
-      if (parts.length == 2) return parts.pop().split(';').shift();
-    }
-    /**
-     *
-     * @param {String} key - Destroy cookie by value.
-     */
-    Document.prototype.deleteCookie = function(key) {
-      document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    }
+  Document.prototype.getCookie = function(key) {
+    let value = '; ' + document.cookie;
+    let parts = value.split('; ' + key + '=');
+    if (parts.length == 2) return parts.pop().split(';').shift();
+  };
+  /**
+   *
+   * @param {String} key - Destroy cookie by value.
+   */
+  Document.prototype.deleteCookie = function(key) {
+    document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  };
 }
 
-var temp_backend_base_url;
-var temp_frontend_base_url;
+let temp_backend_base_url;
+let temp_frontend_base_url;
 
 console.log("In environment settings");
 console.log("domain:");
 console.log(document.domain);
 //if in localhost
 if (document.domain == "localhost") {
-    console.log("settign local urls!")
-    temp_backend_base_url = "https://pure-inlet-98383.herokuapp.com/";
-    temp_frontend_base_url = "http://localhost:4000/";
-    console.log(temp_backend_base_url);
+  console.log("settign local urls!");
+  temp_backend_base_url = "https://pure-inlet-98383.herokuapp.com/";
+  temp_frontend_base_url = "http://localhost:4000/";
+  console.log(temp_backend_base_url);
 } else if (document.domain == "ohtukisalli.github.io") {
-    console.log("Running in: " + document.domain);
-    temp_backend_base_url = "https://pure-inlet-98383.herokuapp.com/";
-    temp_frontend_base_url = "https://ohtukisalli.github.io/dev-frontend/";
+  console.log("Running in: " + document.domain);
+  temp_backend_base_url = "https://pure-inlet-98383.herokuapp.com/";
+  temp_frontend_base_url = "https://ohtukisalli.github.io/dev-frontend/";
 //if in production
 } else if (document.domain == "beta.kisallioppiminen.fi") {
-    console.log("Running in: " + document.domain);
-    temp_backend_base_url = "http://kurssihallinta.kisallioppiminen.fi/";
-    temp_frontend_base_url = "http://beta.kisallioppiminen.fi/";
+  console.log("Running in: " + document.domain);
+  temp_backend_base_url = "http://kurssihallinta.kisallioppiminen.fi/";
+  temp_frontend_base_url = "http://beta.kisallioppiminen.fi/";
 } else {
-console.log("ERROR: cannot set temp_backend_base_url and temp_frontend_base_url. Domain changed?");
+  console.log("ERROR: cannot set temp_backend_base_url and temp_frontend_base_url. Domain changed?");
 }
 
 const BACKEND_BASE_URL = temp_backend_base_url;
